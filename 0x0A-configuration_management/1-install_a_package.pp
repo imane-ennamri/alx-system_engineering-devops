4697290a1d010b717ac18a948f1df24b3cad0dc1
imane-ennamri <imaneennamri209@gmail.com>
2024-03-22 23:41:47 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,6 @@
-package { 'python3':
-  ensure => installed,
-}
-
-package { 'werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-}
-
-package { 'flask':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
